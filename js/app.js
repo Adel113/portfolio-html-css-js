b6ccc0ee0553859c6171b971dd253aa0dde2f6aa
Adel SIDI AHMED <adelsidiahmed2020@gmail.com>
2023-08-23 23:11:26 +0200

    j'ai bien structuré la pages, et j'ai ajouté une navbar bien compléte

--- js/app.js
+++ js/app.js
@@ -1,14 +1,47 @@
+const mobileScreen = window.matchMedia("(max-width: 990px )");
+$(document).ready(function () {
+    $(".dashboard-nav-dropdown-toggle").click(function () {
+        $(this).closest(".dashboard-nav-dropdown")
+            .toggleClass("show")
+            .find(".dashboard-nav-dropdown")
+            .removeClass("show");
+        $(this).parent()
+            .siblings()
+            .removeClass("show");
+    });
+    $(".menu-toggle").click(function () {
+        if (mobileScreen.matches) {
+            $(".dashboard-nav").toggleClass("mobile-show");
+        } else {
+            $(".dashboard").toggleClass("dashboard-compact");
+        }
+    });
+});
 
-document.addEventListener("DOMContentLoaded", function () {
-  var animationElement = document.querySelector(".scroll-animation");
 
-  var waypoint = new Waypoint({
-    element: animationElement,
-    handler: function () {
-      animationElement.classList.add("bounce-top");
-      this.destroy(); // Arrête le suivi une fois l'animation ajoutée
-    },
-    offset: "50%" // L'animation se déclenchera lorsque l'élément est à 50% de la fenêtre
+
+document.addEventListener('DOMContentLoaded', function() {
+  const CompetencesLink = document.getElementById('CompétencesLink');
+  const competencesSection = document.getElementById('competencesSection');
+  
+  CompetencesLink.addEventListener('click', function(event) {
+      event.preventDefault(); // Empêcher le comportement par défaut du lien
+      
+      // Utiliser smooth scrolling pour faire défiler jusqu'à la section compétences
+      competencesSection.scrollIntoView({ behavior: 'smooth' });
   });
 });
+document.addEventListener('DOMContentLoaded', function() {
+  const FormationsLink = document.getElementById('FormationsLink');
+  const competencesSection = document.getElementById('FormationsSection');
+  
+  FormationsLink.addEventListener('click', function(event) {
+      event.preventDefault(); // Empêcher le comportement par défaut du lien
+      
+      // Utiliser smooth scrolling pour faire défiler jusqu'à la section compétences
+      competencesSection.scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+
 
