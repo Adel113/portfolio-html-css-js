70664f8726463c874384e923948fe2ad1702c833
Adel SIDI AHMED <adelsidiahmed2020@gmail.com>
2023-08-24 01:07:09 +0200

    j'ai bien structuré la pages, et j'ai ajouté une navbar bien compléte + Bientôt fini !

--- js/app.js
+++ js/app.js
@@ -31,17 +31,55 @@ document.addEventListener('DOMContentLoaded', function() {
       competencesSection.scrollIntoView({ behavior: 'smooth' });
   });
 });
+
 document.addEventListener('DOMContentLoaded', function() {
   const FormationsLink = document.getElementById('FormationsLink');
-  const competencesSection = document.getElementById('FormationsSection');
+  const FormationsSection = document.getElementById('FormationsSection');
   
   FormationsLink.addEventListener('click', function(event) {
       event.preventDefault(); // Empêcher le comportement par défaut du lien
       
-      // Utiliser smooth scrolling pour faire défiler jusqu'à la section compétences
-      competencesSection.scrollIntoView({ behavior: 'smooth' });
+      // Utiliser smooth scrolling pour faire défiler jusqu'à la section Formations
+      FormationsSection.scrollIntoView({ behavior: 'smooth' });
   });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  const CoordonnéesLink = document.getElementById('CoordonnéesLink');
+  const CoordonnéesSection = document.getElementById('CoordonnéesSection');
+  
+  CoordonnéesLink.addEventListener('click', function(event) {
+      event.preventDefault(); // Empêcher le comportement par défaut du lien
+      
+      // Utiliser smooth scrolling pour faire défiler jusqu'à la section Coordonnées
+      CoordonnéesSection.scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+
+
+// Fonction pour vérifier si l'élément est visible dans la fenêtre de visualisation
+function isElementInViewport(element) {
+  var rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Fonction pour ajouter la classe "bounce-top" si l'élément est visible
+function animateOnScroll() {
+  var element = document.querySelector('.animation-trigger');
+  if (isElementInViewport(element)) {
+    element.classList.add('bounce-top');
+  }
+}
+
+// Écouteur d'événement pour déclencher l'animation lorsque l'utilisateur fait défiler
+window.addEventListener('scroll', animateOnScroll);
+
 
 
 
